2bc61c6d6952d6a08c5d0df63d936f94d6ffcc37
hramzy <hramzy@bol.com>
2022-11-08 04:45:40 +0100

    comment displaying sql queries on running it and add logs to ExceptionHandlerController

--- src/main/java/com/abn/amro/recipes/resource/ExceptionHandlerController.java
+++ src/main/java/com/abn/amro/recipes/resource/ExceptionHandlerController.java
@@ -1,6 +1,8 @@
 package com.abn.amro.recipes.resource;
 
 import static com.abn.amro.recipes.utils.ErrorEnum.GENERAL_ERROR;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -10,15 +12,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice()
 public class ExceptionHandlerController {
 
+    Logger logger = LoggerFactory.getLogger(ExceptionHandlerController.class);
+
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<String> handleRuntimeException(RuntimeException e) {
-        e.printStackTrace(); // TODO :: remove
+        logger.error(e.getMessage());
         return ResponseEntity.badRequest().body(e.getMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        e.printStackTrace();
+        logger.error(e.getMessage());
         String message = e.getBindingResult().getFieldErrors().stream()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .findFirst().orElse(GENERAL_ERROR.getMessage());
@@ -27,7 +31,7 @@ public class ExceptionHandlerController {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception e) {
-        e.printStackTrace();
+        logger.error(e.getMessage());
         return ResponseEntity.status(500).body(e.getMessage());
     }
 
